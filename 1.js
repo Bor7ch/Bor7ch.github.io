78ee4e6a48467df3649f725c1861ac8fe027dc66
Bor7ch <41537284+Bor7ch@users.noreply.github.com>
2018-08-10 22:42:18 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,17 +1,13 @@
 document.getElementById('myslide').onmousemove = function (event){
-    document.getElementById('myslide').ontouchmove = function (event){
-    document.getElementById('two').style.transition = 'all 0.05s';
-    var x = event.offsetX;
-    }
     document.getElementById('two').style.transition = 'all 0.05s';
     var x = event.offsetX; 
     console.log(x);
     document.getElementById('two').style.width = x + 'px';
 }
-    document.getElementById('myslide').onmouseleave = function (event){
-        document.getElementById('two').style.width = '700px';
-        document.getElementById('two').style.transition = 'all 0.5s';
-    }
+ document.getElementById('myslide').onmouseleave = function (event){
+    document.getElementById('two').style.width = '700px';
+    document.getElementById('two').style.transition = 'all 0.5s';
+}
     
 
 
