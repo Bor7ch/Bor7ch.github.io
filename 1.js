9107d7d85d56d5839a88cc1729707be12d9a94ae
Bor7ch <41537284+Bor7ch@users.noreply.github.com>
2018-08-10 21:30:30 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,12 @@
+document.getElementById('myslide').onmousemove = function (event){
+    document.getElementById('two').style.transition = 'all 0.05s';
+    var x = event.offsetX; 
+    console.log(x);
+    document.getElementById('two').style.width = x + 'px';
+}
+    document.getElementById('myslide').onmouseleave = function (event){
+        document.getElementById('two').style.width = '700px';
+        document.getElementById('two').style.transition = 'all 0.5s';
+    }
+    
+
